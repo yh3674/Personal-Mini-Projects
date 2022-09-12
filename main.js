22e9d0a1ff2de75d0891ad6184842acbaf2dc471
yh3674git config --global user.name <yoonhk3674@gmail.com>
2022-09-12 18:48:08 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//1. Creating two box 
+//2. w/ drop-down lists
+//3. Crawling exchange informations from google
+//4. Currancy change
+
+// Crawling exchange informations from google
+let CurrancyRatio = { //var
+    USD:{},
+    KRW:{},
+    VND:{}
+} 
